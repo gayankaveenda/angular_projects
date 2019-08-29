375e5bdff9ad5e53a0a1dcd21964cb27b23da35f
Gayan Kaveenda <gayankaveenda@gmail.com>
2019-08-29 22:14:08 +1000

    recipe component added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('Apple Pie', 'Best quick dessert',
+      'https://img.taste.com.au/Jz8OLy7E/w643-h428-cfill-q90/taste/2016/11/classic-apple-pie-84181-1.jpeg')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
