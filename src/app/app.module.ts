f060a2d8468e35aeac22f2e9017cf989cabd442f
Gayan Kaveenda <gayankaveenda@gmail.com>
2019-08-29 20:40:35 +1000

    added project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
 import {FormsModule} from "@angular/forms";
 import { UserNameComponent } from './user-name/user-name.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
     ServerComponent,
-    UserNameComponent
+    UserNameComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
